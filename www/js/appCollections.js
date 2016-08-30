6d812c03c1fc34e310ce73123212d458a4bd4e75
ted <linyc1983@gmail>
2016-08-30 10:36:39 +0800

    modify css

--- www/js/appCollections.js
+++ www/js/appCollections.js
@@ -544,7 +544,11 @@ ava.views.Table_New_Customize_Collection = ava.views.Table_New_Collection.extend
 	            			}
 
 	            			var tableHeight = $(window).height() -2 -$("div[data-role=footer]").outerHeight() - $('#RealtimeInfo_Today_Test-table thead').height();
+	            			if( tableHeight < 56){
+								tableHeight = 56;
+							}
 		        			$('#RealtimeInfo_Today_Test-table tbody').css('height',tableHeight.toString());
+	            			
 	            		}
 	            	});
 	            	$('.pinned #RealtimeInfo_Today_Test-table').show();
